e31b5e8f430e8f4f6e0ec1ed6019e5b4b0d43cee
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-05-18 12:27:15 -0700

    Remove unused code and add README with usage instructions

--- main_app.cpp
+++ main_app.cpp
@@ -1,13 +1,6 @@
 #include <iostream>
 
-namespace first {
-int x = 0;
-}
-
 int main() {
-    std::string name = "Bro";
-
-    std::cout << name;
 
     // Formatting for zsh
     std::cout << std::endl;
