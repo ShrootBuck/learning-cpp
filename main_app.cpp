aa393dfc5f2fc34e00d49c3acf50c37142968034
ShrootBuck <ShrootBuck@users.noreply.github.com>
2025-05-18 12:31:58 -0700

    Add .gitignore and greet user by name in main_app

--- main_app.cpp
+++ main_app.cpp
@@ -2,6 +2,12 @@
 
 int main() {
 
+    std::string name;
+
+    std::getline(std::cin >> std::ws, name);
+
+    std::cout << "Hello " << name << "!";
+
     // Formatting for zsh
     std::cout << std::endl;
     return 0;
